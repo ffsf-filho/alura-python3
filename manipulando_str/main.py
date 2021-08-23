8c4c76951e66730b581f8c93c5f20999ff70c1c0
Francisco Florentino de Sousa Filho <ffsf.filho@gmail.com>
2021-08-23 12:41:28 -0300

    Inclusão do projeto básico manipulando_str para uso no módulo Parte 05

---
+++ manipulando_str/main.py
@@ -0,0 +1,12 @@
+sobre_mim = "Meu nome é Rodrigo e minha idade é 26"
+meu_nome = "Rodrigo"
+
+sub_string = meu_nome[4]
+print(sub_string)
+
+sub_string = meu_nome[2:6]
+print(sub_string)
+
+sub_string = sobre_mim[35:]
+print(sub_string)
+argumento ="https://www.bytebank.com.br/cambio?moedaorigem=real&moedadestino=dolar&valor=1500"
