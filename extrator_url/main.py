66f8f58624a8a58118c13c39d3a6edca5e63ea78
Francisco Florentino de Sousa Filho <ffsf.filho@gmail.com>
2021-08-29 14:52:29 -0300

    Conclusão do Módulo Parte 05B - String em Python, extraindo informações de uma URL

--- extrator_url/main.py
+++ extrator_url/main.py
@@ -1,8 +1,29 @@
-url = "bytebank.com/cambio?moedaOrigem=real"
-print(url)
+# Arquivo utilizado até a aula 3, quando então passamos a utilizar a classe
+# ExtratorURL no arquivo extrator_url.py
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
 
-url_base = url[0:19]
-print(url_base)
+# Sanitização da URL
+url = url.strip()
 
-url_parametros = url[20:36]
+# Validação da URL
+if url == "":
+    raise ValueError("A URL está vazia")
+
+# Separa base e parâmetros
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
+
+# Busca o valor de um parâmetro
+parametro_busca = 'quantidade'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+
+print(valor)
+
